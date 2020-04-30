04d9fa1c07e87c3935168c2ec29ecf1e3d9f0d80
李星阳 <176840078@qq.com>
2020-04-30 12:44:07 +0800

    公司提交

--- 04 string.go
+++ 04 string.go
@@ -16,7 +16,7 @@ func main() {
 	fmt.Println("-------------------------")
 	fmt.Println(`字符串长度：`, len(s02))
 	// fmt.Println(`字符串长度：`, utf8.RuneCountInString(s02))
-	fmt.Println(`字符串拼接：`, s02+s02)
+	fmt.Println(`字符串拼接：`, s02+s03)
 	fmt.Println(fmt.Sprintf(`字符串模板： 欢迎“%s”今天第“%s”次登录`, "小明", "3"))
 	fmt.Println(`字符串分割：`, strings.Split("分,割", ",")) //分割为数组
 	fmt.Println(`字符串包含：`, strings.Contains(s02, "字"))
---
+++ practice.md
@@ -0,0 +1,10 @@
+<!--
+ * @Author: 李星阳
+ * @Date: 2020-04-28 13:38:58
+ * @LastEditors: 李星阳
+ * @LastEditTime: 2020-04-28 13:47:45
+ * @Description: 
+ -->
+# 变量
+多行声明
+
