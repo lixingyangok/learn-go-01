cf6654278f9f802e17cc4b4b9a74932add216654
lixingyang <176840078@qq.com>
2020-05-03 21:39:25 +0800

    李：字符相关练习

--- training-history/01/string-01.go
+++ training-history/01/string-01.go
@@ -8,8 +8,8 @@ import (
 var pt = fmt.Println
 
 func main() {
-	var s01 = "磨穿铁砚-天道酬勤-"
-	var s02 = "磨穿铁砚-天道酬勤-"
+	var s01 = "磨穿铁砚-"
+	var s02 = "天道酬勤-"
 	var splitStr = strings.Split(s01, "-")
 	pt("字节长度：", len(s01))
 	pt("字符长度：", len([]rune(s01)))
@@ -22,4 +22,12 @@ func main() {
 	pt("查看是否某字结尾：", strings.HasSuffix(s01, "勤"))
 	pt("查看某字所在索引：", strings.Index(s01, "-"))
 	pt("查看某字所在索引（倒序：", strings.LastIndex(s01, "-"))
+	for idx, val := range s01 {
+		fmt.Println(idx, "-", val, "-", string(val))
+	}
+	var numToStr01 = string(30952) // 真·转文字
+	var numToStr02 = fmt.Sprintf("%v", 123.456) // 数字转文字（字符型数字
+	pt("数字转文字：", numToStr01)
+	pt("数字转文字：", numToStr02)
 }
+
