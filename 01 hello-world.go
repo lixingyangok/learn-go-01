b2f3468d40fa888293c1ea7228b345262b7cdbd2
李星阳 <176840078@qq.com>
2020-05-06 13:48:14 +0800

    李：短声明不能放在主函数外

--- 01 hello-world.go
+++ 01 hello-world.go
@@ -2,7 +2,7 @@
  * @Author: 李星阳
  * @Date: 2020-04-22 12:09:49
  * @LastEditors: 李星阳
- * @LastEditTime: 2020-05-02 13:17:44
+ * @LastEditTime: 2020-05-06 13:47:52
  * @Description: 
  */
 
@@ -11,8 +11,12 @@ package main
 import "fmt"
 
 // 函数外只能放标识符，即：声明变量、声明常量、声明函数、声明类型，等各种声明
-// fmt.Println  // 此为非法语句，不能放在这里（主函数外）
 var helloWorld = "hello world" // 合法声明
+
+/*
+fmt.Println()	//此为非法语句，不能放在这里（主函数外）
+num01 := 100	//短声明只能放在主函数内
+*/
 
 func main() {
 	fmt.Println(helloWorld)
