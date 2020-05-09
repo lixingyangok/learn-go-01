df1597cf4a5c322e60e59a63bbb7995f624684f8
李星阳 <176840078@qq.com>
2020-05-09 13:51:19 +0800

    两数相除得值研究

--- the-key/01 int-and-float.go
+++ the-key/01 int-and-float.go
@@ -1,10 +1,29 @@
+/*
+ * @Author: 李星阳
+ * @Date: 2020-05-09 10:48:19
+ * @LastEditors: 李星阳
+ * @LastEditTime: 2020-05-09 13:46:32
+ * @Description: 
+ */
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main() {
-	var slice01 = [...]int{1,2,3}
-	var aa = len(slice01)
-	var bb = float32(aa) / 2
-	fmt.Println(aa, bb)
+	var aa = 3 / 2 // =1, int型
+	fmt.Println("3/2 =", aa, reflect.TypeOf(aa), "\n")  // =1，int型
+
+	var bb = float32(3) / 2 //float32型
+	fmt.Println("float32(3) / 2 = ", bb, "\n")
+
+
+	fmt.Println("3/1.5 =", 3 / 1.5, reflect.TypeOf(3/1.5)) // =2，float64型
+	fmt.Println("3/1.0 =", 3 / 1.0, reflect.TypeOf(3/1.0)) // =3，float64型
+	fmt.Println(
+		reflect.TypeOf(aa), "\n",
+		reflect.TypeOf(bb),
+	)
 }
