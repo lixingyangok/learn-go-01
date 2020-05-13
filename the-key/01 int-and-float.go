dcb4660170e295b46c2944edd75e01cad6cba2c3
lixingyang <176840078@qq.com>
2020-05-13 21:33:34 +0800

    跨类型运算研究

--- the-key/01 int-and-float.go
+++ the-key/01 int-and-float.go
@@ -25,7 +25,12 @@ func main() {
 	fmt.Println("有一浮点得浮点：", 3.0/1, 3/1.0, 3.0*1, 3*1.0) //通过 reflect.TypeOf() 它们便是 float64
 
 	var n01 = 3
-	fmt.Println(n01 * 1.0 / 2) //1
-	fmt.Println(3 * 1.0 / 2)  //1.5
+	fmt.Println(n01 * 1.0 / 2) // 1     如果左侧运算符类型确定，则右侧转为左侧类型再运算
+	fmt.Println(3 * 1.0 / 2)   // 1.5   如果左侧类型不确定，则根据右侧类型推导左侧类型
+	/*
+		几乎所有字面量（literal）都是untyped 
+		如有一个 untyped 跟一个 非untyped 运算时
+		会转换到 非untyped 的类型 
+	*/
 
 }
