e79bddaa1d911b445911ec04f14050ef335ad6a5
李星阳 <176840078@qq.com>
2020-05-22 14:20:56 +0800

    复制切片

--- 08 slice .go
+++ 08 slice .go
@@ -5,8 +5,8 @@ import "fmt"
 func main() {
 	var a01 = [...]int{0, 1, 2, 3, 4} //数组
 	var s01 = a01[2:]                 //从数组生成切片。[]内:符号，左包含右不包含
-	var s02 = []int{}                 //新建空切片
-	var s03 []int                     //没申请内存
+	var s02 = []int{}                 //【空切片】已申请内存
+	var s03 []int                     //【空切片】但没申请内存
 	fmt.Println(
 		s01,
 		len(s01), //切片长度
@@ -17,15 +17,17 @@ func main() {
 		s02, "\n",
 		s03, "\n",
 	)
-
 	fmt.Println(
 		a01[:], "\n", //从头取到尾
 		a01[2:], "\n", //2参不传取到最后
 		a01[:2], "\n", //1参不传从头取
 	)
-
-	var s02 = []string{"北京", "上海", "广州"}
-	s02 = append(s02, "深圳") //切片添加一个成员
-	fmt.Println(s02)
-
+	var s04 = []string{"北京", "上海", "广州"}
+	var s05 = make([]string, len(s04)) // 通过 make 函数申请内存
+	s04 = append(s04, "深圳") //切片添加一个成员
+	copy(s05, s04) //长度4的切片，覆盖给长度3的切片，没成功
+	fmt.Println(s04)
+	fmt.Println(s05)
 }
+
+
