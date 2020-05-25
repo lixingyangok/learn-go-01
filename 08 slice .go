a4f3f9aaff7bf43a784d6c7c1fffa9da0d0b9454
李星阳 <176840078@qq.com>
2020-05-25 14:02:23 +0800

    删除切片成员

--- 08 slice .go
+++ 08 slice .go
@@ -23,11 +23,19 @@ func main() {
 		a01[:2], "\n", //1参不传从头取
 	)
 	var s04 = []string{"北京", "上海", "广州"}
-	var s05 = make([]string, len(s04)) // 通过 make 函数申请内存
+	var s05 = make([]string, len(s04), 99) // 通过 make 函数申请内存
+	var s05b = []string{} //有内存空切片
 	s04 = append(s04, "深圳") //切片添加一个成员
-	copy(s05, s04) //长度4的切片，覆盖给长度3的切片，没成功
+	copy(s05, s04) //长度4的切片，覆盖给长度3的切片，最终长度=3
+	s05b = append(s05b, s04...)
+	s05b[0] = "南京"
 	fmt.Println(s04)
 	fmt.Println(s05)
+	fmt.Println(s05b)
+
+	var s06 = make([]string, len(s05b))
+	s06 = append(s05b[:1], s05b[2:]...)
+	fmt.Print(s06)
 }
 
 
