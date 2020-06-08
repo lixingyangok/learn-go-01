be47d0fd8958332a4502d09cf7c3efe1f8809455
lixingyang <176840078@qq.com>
2020-06-08 14:01:44 +0800

    分金币练习

---
+++ training/xx gold-coins.go
@@ -0,0 +1,46 @@
+package main
+
+import "fmt"
+
+var coins = 100
+var nameMap = map[string]int{
+	"Monday":    0,
+	"Tuesday":   0,
+	"Wednesday": 0,
+	"Thursday":  0,
+	"Friday":    0,
+	"Saturday":  0,
+	"Sunday":    0,
+}
+var rule = map[string]int{
+	"a": 1,
+	"b": 2,
+	"c": 3,
+	"d": 4,
+	"e": 5,
+}
+func getHow(key string) (how int) {
+	for _, curLetter := range key {
+		fmt.Println(string(curLetter))
+	}
+	return;
+}
+
+func main() {
+	// fmt.Println(coins)
+	// fmt.Println(nameMap)
+	// fmt.Println(rule)
+	for key, _ := range nameMap {
+		getHow(key)
+		// fmt.Printf("%T  ", key)
+		// iHowMany, ok := rule[key]
+		// if ok != nil {
+		// 	continue
+		// }
+		// cur[key] = cur[key] + 1
+		// coins--;
+		// fmt.Println(key, cur)
+	}
+	// fmt.Println(nameMap)
+	
+}
