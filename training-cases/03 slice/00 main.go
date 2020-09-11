eadbd7ef567e3a89b97c3f136d52db82405eca5f
李星阳 <176840078@qq.com>
2020-09-11 17:08:40 +0800

    李：准备练习切片

---
+++ training-cases/03 slice/00 main.go
@@ -0,0 +1,12 @@
+/*
+ * @Author: 李星阳
+ * @Date: 2020-09-11 11:21:22
+ * @LastEditors: 李星阳
+ * @LastEditTime: 2020-09-11 11:21:31
+ * @Description: 
+ */
+package main
+
+func main() {
+	// 每个文件夹都需要一个非 ignore 的文件，所以有这个文件
+}
---
+++ training-cases/03 slice/01 slice.go
@@ -0,0 +1,14 @@
+// +build ignore
+
+package main
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	// ▼制造一个数组，有7个成员，分别是下周的7天
+	var slice01 = [7]string
+
+
+}
