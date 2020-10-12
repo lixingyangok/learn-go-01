74cde2f8ac187a64e73c055a57c250550c4d309d
lixingyang <176840078@qq.com>
2020-10-12 21:05:09 +0800

    提交：打印八卦图

--- training-cases/06 bagua/01.go
+++ training-cases/06 bagua/01.go
@@ -4,28 +4,24 @@ package main
 
 import (
 	"fmt"
-	// "math"
 )
 
 func main() {
-
-	// fmt.Println(symbol01)
-	// fmt.Println(symbol02)
-	// fmt.Println(symbol01)
-	var slice01 = make([][3]int, 0, 10)
-	// var flag = 0
+	// 注意：make的2参是0，如果非0，则生成的切片会有默认的成员
+	// 3参可以省略
+	var slice01 = make([][3]int, 0, 10) // 保存由0、1两个数字能形成的各种组合
 	for idx := 0; idx <= 1; idx++ {
 		for idx02 := 0; idx02 <= 1; idx02++ {
 			for idx03 := 0; idx03 <= 1; idx03++ {
-				var newOne = [3]int{idx, idx02, idx03}
-				slice01 = append(slice01, newOne)
-				// slice01[flag] = newOne
-				// flag++
-				// fmt.Println(idx, idx02, idx03)
+				// var newOne = [3]int{idx, idx02, idx03}
+				// slice01 = append(slice01, newOne)
+				// ▲注意上下2种插入方法▼
+				slice01 = append(slice01, [3]int{idx, idx02, idx03})
 			}
 		}
 	}
-	var slice02 = make([]string, 0, 10)
+
+	var slice02 = make([]string, 0, 10) // 数组形式转字符形式
 	var yang = "■■■■■■■"
 	var yin = "■■■ ■■■"
 	for _, curArr := range slice01 {
@@ -35,7 +31,7 @@ func main() {
 			if curVal == 1 {
 				mark = yang
 			}
-			if (idx+1 < len(curArr)) {
+			if idx+1 < len(curArr) {
 				mark += "\n"
 			}
 			symbol += mark
