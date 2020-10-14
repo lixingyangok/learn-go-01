908ae426d6374143010251fc7d5a7167e218a2e7
李星阳 <176840078@qq.com>
2020-10-14 16:16:06 +0800

    初步打印“金钱课

--- training-cases/07 coins-map/01.go
+++ training-cases/07 coins-map/01.go
@@ -6,6 +6,15 @@ import (
 	"fmt"
 )
 
+func numToString(num int) (string){
+	if num == 1 {
+		return "●"
+	}else if num == 0 {
+		return "○"
+	}
+	return "-"
+}
+
 func main() {
 	// var slice01 = make([]string, 0)
 	var flag = 1
@@ -15,10 +24,8 @@ func main() {
 				for i04 := 0; i04 <= 1; i04++ {
 					for i05 := 0; i05 <= 1; i05++ {
 						for i06 := 0; i06 <= 1; i06++ {
-							// fmt.Println(flag, "：", i01,i02,i03,i04,i05,i06)
-							var aa, err = fmt.Printf("%v：%v%v%v%v%v%v\n", flag, i01, i02, i03, i04, i05, i06)
-							fmt.Println(aa, err)
-							// slice01 = append(slice01, )
+							var coinsList = numToString(i01) + numToString(i02) + numToString(i03) + numToString(i04) + numToString(i05) + numToString(i06)
+							fmt.Println(flag, coinsList)
 							flag++
 						}
 					}
