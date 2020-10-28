db6806551ecaf564af2d6f94f3d87e81e8a5ef4f
lixingyang <176840078@qq.com>
2020-10-28 22:12:09 +0800

    练习案例

--- training-cases/02 dispense-coins/04 dispense-coins.go
+++ training-cases/02 dispense-coins/04 dispense-coins.go
@@ -2,10 +2,47 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 // 2020.09.14 8:7:36 星期一
 
+var myMap = map[string]int{
+	"张": 3,
+	"李": 4,
+	"王": 5,
+	"赵": 6,
+	"马": 9,
+}
+
+func fn01 (total int, theSlice []string) (map[string]int, int) {
+	result := map[string]int{}
+	for _, curName := range theSlice {
+		needToGive := 0
+		for curFamilyName, curNum := range myMap {
+			if strings.HasPrefix(curName, curFamilyName) {
+				needToGive = curNum
+				continue
+			}
+		}
+		result[curName] = needToGive
+		total -= needToGive
+	}
+	return result, total
+}
 
 func main() {
-	
+	result, rest := fn01(25, []string{
+		"张三",
+		"张仨",
+		"李四",
+		"马英九",
+		"王五",
+		"赵六",
+		"司马七",
+		"诸葛八",
+	})
+	fmt.Println(rest)
+	fmt.Println(result)
 }
