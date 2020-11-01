8712fd06aca8599a2889967563f2adebcb555a4a
lixingyang <176840078@qq.com>
2020-11-01 17:02:46 +0800

    补充注释

--- 09 map.go
+++ 09 map.go
@@ -12,12 +12,13 @@ func main() {
 	// 声明空map，指定长度，用make
 	// 声明同时初始化map
 	// 判断某键是否存在
-	var m00 map[int]string
-	var m01 = map[int]string{}
-	var m02 = make(map[int]string, 8) //有8个键值对？
-	fmt.Println(m00, m00 == nil)      // 是nil
-	fmt.Println(m01, m01 == nil)      // 不是nil
-	fmt.Println(m02, m02 == nil)      // 不是nil
+	var m00 map[int]string                    // nil
+	var m01 = map[int]string{}                // not a nil 
+	var m02 = make(map[int]string, 8)         //有8个键值对？
+	var s01 = make([]map[string]string, 3, 3) // 生成三个nil
+	fmt.Println(m00, m00 == nil)              // 是nil
+	fmt.Println(m01, m01 == nil)              // 不是nil
+	fmt.Println(m02, m02 == nil)              // 不是nil
 
 	// m00[1] = "wrong" //报错，因为没有初始化
 	m01[1] = "OK"
@@ -55,8 +56,7 @@ func main() {
 	}
 	fmt.Println(m12)
 
-
 	// 声明一个值为切片的map，然后不能立即赋值，如何解决？
 	// 例：How do you do? 统计各个单词出现的数量
-	
+
 }
