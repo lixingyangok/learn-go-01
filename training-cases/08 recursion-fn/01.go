4d00c6376c316e4da10592f2d68f6796b991ce1c
李星阳 <176840078@qq.com>
2020-11-03 14:12:59 +0800

    递归打印一段数字区间

--- training-cases/08 recursion-fn/01.go
+++ training-cases/08 recursion-fn/01.go
@@ -6,9 +6,27 @@ import (
 	"fmt"
 )
 
+func printNumberToSmall(number int){
+	if number > 0 {
+		fmt.Print(number, "、")
+		printNumberToSmall(number - 1)
+	}
+}
+
+func printNumberToBig(n1, n2 int){
+	fmt.Print(n1, "、")
+	if n1 < n2 {
+		printNumberToBig(n1 + 1, n2)
+	}
+}
+
 func main() {
 	// 递归打印1-5
+	printNumberToSmall(5);
+	fmt.Println("\n--------------------")
+	printNumberToBig(1, 5);
+
+	
 	// 递归求和1-10
 	// 递归5的阶乘
-	fmt.Println("123")
 }
