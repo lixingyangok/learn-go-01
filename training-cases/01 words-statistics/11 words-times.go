90e6bfb7a325732af577157eeeb2f9665ff92350
李星阳 <176840078@qq.com>
2021-01-09 18:06:29 +0800

    李：补充为

---
+++ training-cases/01 words-statistics/11 words-times.go
@@ -0,0 +1,30 @@
+// +build ignore
+
+package main
+
+import (
+	// "fmt"
+	// "strings"
+)
+
+// func fn01 (sentence string) map[string]int {
+// 	wordsSlice := strings.Split(sentence, " ")
+// 	result := map[string]int{}
+// 	// fmt.Println("立即打印：", result)
+// 	for _, curWord := range wordsSlice {
+// 		times, _ := result[curWord]
+// 		// fmt.Println(times)
+// 		// ▲不用担心，至少也能打印出0
+// 		if times > 0 {
+// 			result[curWord]++
+// 		}else {
+// 			result[curWord] = 1
+// 		}
+// 	}
+// 	return result
+// }
+
+func main() {
+	// result := fn01("hello how are you are you ok")
+	// fmt.Println(result)
+}
--- training-cases/10 letter-group.go/00.go
+++ training-cases/10 letter-group.go/00.go
@@ -2,12 +2,13 @@
  * @Author: 李星阳
  * @Date: 2020-11-18 17:44:21
  * @LastEditors: 李星阳
- * @LastEditTime: 2020-12-01 20:55:43
+ * @LastEditTime: 2021-01-09 16:42:47
  * @Description:
  */
 
 package main
 
+// 检测某几个字母，在某个长度下最多能产生多少个组合
 func main() {
 
 }
